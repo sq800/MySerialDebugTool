feaa9854d720045c6690eb9d359d30949e0eb763
Zhenjiang Wang <wangzj_0826@163.com>
2022-06-30 15:53:11 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+using System.Windows.Threading;
+using System.IO;
+
+namespace My串口调试助手
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //实例化一个定时器
+        DispatcherTimer dtimer = new DispatcherTimer();
+        //实例化一个串口类
+        private MyCom mCom = new MyCom();
+        //日志功能是否开启
+        private bool openLog = false;
+        public MainWindow()
+        {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //开启定时器
+            //设置定时间隔为100ms
+            dtimer.Interval = TimeSpan.FromMilliseconds(100);
+            //注册定时中断事件
+            dtimer.Tick += new EventHandler(timer_Tick);
+            dtimer.Start();
+
+            //搜索串口
+            string[] ports = SerialPort.GetPortNames();
+            //添加到串口列表
+            this.portsName.ItemsSource = ports;
+            //设置默认选项，索引方式
+            this.portsName.SelectedIndex = 3;
+
+            //设置默认波特率
+            string[] baudRateList = new string[] { "300", "600", "1200", "2400", "4800", "9600", "19200", "38400", "43000", "56000", "57600", "115200" };
+            this.baudRate.ItemsSource = baudRateList;
+            this.baudRate.SelectedItem = "9600";
+
+            //添加选项--7位数据位
+            this.dataBit.Items.Add(7);
+            this.dataBit.Items.Add(8);
+            this.dataBit.SelectedIndex = 1;
+            //MessageBox.Show(this.dataBit.Items.ToString());
+
+            //设置默认停止位
+            this.stopBit.SelectedIndex = 0;
+
+            //设置默认校验位
+            this.checkBit.SelectedIndex = 0;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if(SerialCom.comData.Count > 0)
+            {
+                string str = SerialCom.comData[0] + " ";
+                revData.Text += str;
+                //移除索引0处的数据，后面的数据会重新排序
+                SerialCom.comData.RemoveAt(0);
+                //滚动到最后一行
+                revData.ScrollToEnd();
+                if (SerialCom.comStatus)
+                {
+                    open.Content = "关闭串口";
+                }
+                else
+                {
+                    open.Content = "打开串口";
+                }
+                //若开启了日志
+                if (openLog)
+                {
+                    WriteLog(str);
+                }
+            }
+        }
+
+        private void search_Click(object sender, RoutedEventArgs e)
+        {
+            //搜索串口
+            string[] ports = SerialPort.GetPortNames();
+            //添加到串口列表
+            this.portsName.ItemsSource = ports;
+            //设置默认选项
+            this.portsName.SelectedIndex = 0;
+        }
+
+
+        private void open_Click(object sender, RoutedEventArgs e)
+        {
+            SerialCom.comName = portsName.Text;
+            SerialCom.comBaudRate = int.Parse(baudRate.Text);
+            SerialCom.comDataBit = int.Parse(dataBit.Text);
+            SerialCom.comCheckBit = checkBit.Text;
+            SerialCom.comStopBit = stopBit.Text;
+            mCom.openCom();
+        }
+
+        private void send_Click(object sender, RoutedEventArgs e)
+        {
+            mCom.WriteData(Encoding.UTF8.GetBytes(sendData.Text));
+            string str = DateTime.Now.ToString().Split(" ")[1];
+            string logStr = str + "->  " + sendData.Text;
+            SerialCom.comData.Add(logStr);
+            //若开启了日志
+            if (openLog)
+            {
+                //WriteLog(logStr);
+            }
+
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("calc.exe");
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("mspaint.exe");
+        }
+
+        //开始日志
+        private void log_Click(object sender, RoutedEventArgs e)
+        {
+            openLog = !openLog;
+            if (openLog)
+            {
+                log.Content = "关闭日志";
+            }
+            else
+            {
+                log.Content = "开启日志";
+            }
+        }
+
+        //写日志
+        private void WriteLog(string str)
+        {
+            // 获取当前文件夹
+            string currentDirName = System.IO.Directory.GetCurrentDirectory();
+            Console.WriteLine(currentDirName);
+            // 创建一个子文件夹名称，并添加到当前文件夹下
+            string pathString = System.IO.Path.Combine(currentDirName, "log");
+            // 创建子文件夹
+            System.IO.Directory.CreateDirectory(pathString);
+
+            // 创建文件名
+            string fileName = "log.txt";
+;
+            // 再次使用 Combine 方法把文件名添加到路径
+            pathString = System.IO.Path.Combine(pathString, fileName);
+
+            // 写
+            using (StreamWriter sw = File.AppendText(pathString))
+            {
+                sw.WriteLine(str);
+
+            }
+        }
+    }
+
+}
---
+++ SerialCom.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO.Ports;
+using System.Threading;
+
+namespace My串口调试助手
+{
+    //串口类
+    public class SerialCom
+    {
+        public static SerialPort com { get; set; } = new SerialPort();
+        //串口名字
+        public static string comName { get; set; }
+        //波特率
+        public static int comBaudRate { get; set; }
+        //校验位
+        public static string comCheckBit { get; set; }
+        //数据位
+        public static int comDataBit { get; set; }
+        //停止位
+        public static string comStopBit { get; set; }
+        //串口打开状态
+        public static bool comStatus { get; set; }
+        //数据显示
+        public static List<string> comData = new List<string>();
+    }
+
+    //串口收发类
+    public class MyCom
+    {
+        //方法：串口打开
+        public void openCom()
+        {
+            if (SerialCom.comStatus == false)
+            {
+                //端口名
+                SerialCom.com.PortName = SerialCom.comName;
+                //波特率
+                SerialCom.com.BaudRate = SerialCom.comBaudRate;
+                //数据位
+                SerialCom.com.DataBits = SerialCom.comDataBit;
+                //校验位
+                if (SerialCom.comCheckBit == "none")
+                    SerialCom.com.Parity = System.IO.Ports.Parity.None;
+                if (SerialCom.comCheckBit == "odd")
+                    SerialCom.com.Parity = System.IO.Ports.Parity.Odd;
+                if (SerialCom.comCheckBit == "even")
+                    SerialCom.com.Parity = System.IO.Ports.Parity.Even;
+                //停止位
+                if (SerialCom.comStopBit == "1")
+                    SerialCom.com.StopBits = System.IO.Ports.StopBits.One;
+                if (SerialCom.comStopBit == "1.5")
+                    SerialCom.com.StopBits = System.IO.Ports.StopBits.OnePointFive;
+                if (SerialCom.comStopBit == "2")
+                    SerialCom.com.StopBits = System.IO.Ports.StopBits.Two;
+                //接收、发送数据回车显示
+                SerialCom.com.NewLine = "\r\n";
+                //启动线程
+                Comthread();
+            }
+            else
+            {
+                //关闭串口
+                SerialCom.comData.Add("关闭串口");
+                SerialCom.com.Close();
+                SerialCom.comStatus = false;
+            }
+        }
+        //方法：线程读取串口数据
+        private void Comthread()
+        {
+            //打开串口
+            SerialCom.com.Open();
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            //设置编码格式
+            SerialCom.com.Encoding = Encoding.GetEncoding("GB2312");
+            //创建一个线程
+            Thread thread = new Thread(ReadData);
+            //设置为后台线程
+            thread.IsBackground = true;
+            //启动线程
+            thread.Start();
+
+        }
+        //方法：串口读取
+        public void ReadData()
+        {
+            SerialCom.comData.Add("打开串口");
+            SerialCom.comStatus = true;
+            while (SerialCom.comStatus)
+            {
+                //延时50ms
+                Thread.Sleep(50);
+                try
+                {
+                    //查询串口中目前保存了多少数据
+                    int n = SerialCom.com.BytesToRead;
+                    byte[] buffer = new byte[n];
+                    SerialCom.com.Read(buffer, 0, n);
+                    if (buffer.Length > 0)
+                    {
+                        string str = Encoding.Default.GetString(buffer);
+                        string dateStr = DateTime.Now.ToString().Split(" ")[1];
+                        SerialCom.comData.Add(dateStr + "-<  " + str);
+                        //SerialCom.comData.Add(str);
+                    }
+                }
+                catch
+                {
+                    SerialCom.comStatus = false;
+                    SerialCom.com.Close();
+                }
+            }
+        }
+
+        //方法：串口写数据
+        public void WriteData(byte[] bytes)
+        {
+            if(SerialCom.comStatus && bytes!=null)
+            {
+                SerialCom.com.Write(bytes,0, bytes.Length);
+            }
+        }
+    }
+}
